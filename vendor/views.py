1ce2c8c29e65a6f2359e18b41068d444851e58c4
chandrabhushann <chandrabhushan.nulu@gmail.com>
2022-11-07 14:14:10 +0000

    index.html updated

--- vendor/views.py
+++ vendor/views.py
@@ -2,4 +2,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the vendor index.")
+    return HttpResponse("<h1> You're at the vendor index.</h1>")
